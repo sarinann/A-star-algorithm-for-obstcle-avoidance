742603e74f47971b76344a5869849996c92c933c
sarinann <sarin.ann10@gmail.com>
2023-04-08 00:38:13 -0400

    new map

--- Phase_2/new_map_phase2_a_star_sarin_aditi.py
+++ Phase_2/new_map_phase2_a_star_sarin_aditi.py
@@ -12,21 +12,24 @@ import math
 
 print("Hi!! \n")
 start_time = time.time()
-threshold = 0.5
-robot_radius = int(input("Enter the radius of the robot \n"))
-print("____________________________________________________________")
-clearance = int(input("Enter the clearance to be maintained around the obstacles \n"))
-print("______________________________________________________________________")
+threshold = 1.5
+# robot_radius = int(input("Enter the radius of the robot \n"))
+# print("____________________________________________________________")
+robot_radius = 10
+clearance = 5
+# clearance = int(input("Enter the clearance to be maintained around the obstacles \n"))
+# print("______________________________________________________________________")
 
-ul = int(input("Enter the velocity of left wheel \n"))
-print("______________________________________________________________________")
-
-ur = int(input("Enter the velocity of right wheel \n"))
-print("______________________________________________________________________")
+# ul = int(input("Enter the velocity of left wheel \n"))
+# print("______________________________________________________________________")
+ul = 30
+ur = 40
+# ur = int(input("Enter the velocity of right wheel \n"))
+# print("______________________________________________________________________")
 
 #Total amount the boundary needs to be bloated by taking into account robot radius and clearance.
 total_bloat = clearance + robot_radius
-
+# total_bloat = total_bloat/10
 # Using OpenCV libraries to create shapes of the polynomials of the obstacles and their map
 def create_polynomial(image, vertices, color, type):
     vertices = vertices.reshape((-1,1,2))
@@ -37,42 +40,26 @@ def create_polynomial(image, vertices, color, type):
 
 def create_map():
     #initializing the map
-    map = np.zeros((2000,6000, 3), dtype="uint8") 
+    map = np.zeros((200,600, 3), dtype="uint8") 
     white = (255,255,255)
-    # Hexagon
-    # hexagon = np.array([[300,50], [365, 87.5], [365,162.5], 
-    #                    [300,200], [235, 162.5], [235, 87.5]], np.int32)
-    # create_polynomial(map, hexagon, white, "polygon")
-    # hexagon_bloat = np.array([[300,45], [369, 87.5], [369,162.5], 
-    #                       [300,205], [235, 162.5], [235, 87.5]], np.int32)
-    # create_polynomial(map, hexagon_bloat, white, "Border")
-
-    # # Triangle
-    # traingle = np.array([[460, 25], [460, 225], [510,125]], np.int32)
-    # create_polynomial(map, traingle, white, "polygon")
-    # triangle_bloat = np.array([[456, 20], [456, 230], [514,125]], np.int32)
-    # create_polynomial(map, triangle_bloat, white, "Border") 
-
     #Circle
-    center = (4000, 1100)
-    radius = 500
+    center = (400, 110)
+    radius = 50
     thickness = total_bloat
     cv.circle(map, center, radius, white, thickness)
-    # circle = np.
-
     # Rectangle
     # cv.rectangle(map, (1.5,0.75), (1.65,2), white, -1)
     # cv.rectangle(map, (1.5,0.75), (1.65,2), white, total_bloat)
     # cv.rectangle(map, (2.5,0), (2.65,1.25), white, -1)
     # cv.rectangle(map, (2.5,0), (2.65,1.25), white, total_bloat)
     # cv.rectangle(map, (-1, -1), (7, 3), white, total_bloat)   
-    cv.rectangle(map, (1500,750), (1650,2000), white, -1)
-    cv.rectangle(map, (1500,750), (1650,2000), white, total_bloat)
-    cv.rectangle(map, (2500,0), (2650,1250), white, -1)
-    cv.rectangle(map, (2500,0), (2650,1250), white, total_bloat)
-    cv.rectangle(map, (-1, -1), (6001, 2001), white, total_bloat)   
+    cv.rectangle(map, (150,75), (165,200), white, -1)
+    cv.rectangle(map, (150,75), (165,200), white, total_bloat)
+    cv.rectangle(map, (250,0), (265,125), white, -1)
+    cv.rectangle(map, (250,0), (265,125), white, total_bloat)
+    cv.rectangle(map, (-1, -1), (601, 201), white, total_bloat)   
 
-    return cv.resize(map, (int(6000/threshold),int(2000/threshold)))
+    return cv.resize(map, (int(600/threshold),int(200/threshold)))
 
 #Creating the map and flipping it about Y-Axis to match the origin points 
 map = create_map()
@@ -95,28 +82,30 @@ def if_obstacle(node):
 def a_star_node_create(total_cost, cost_to_come, parent, node):
     return (total_cost, cost_to_come, parent, node)
 
-start_point_x = input("Enter the x-coordinate of the start point \n")
-start_point_y = input("Enter the y-coordinate of the start point \n")
-start_point_theta = input("Enter the start orientation \n")
-start = (int(start_point_x), int(start_point_y), int(start_point_theta))
+# start_point_x = input("Enter the x-coordinate of the start point \n")
+# start_point_y = input("Enter the y-coordinate of the start point \n")
+# start_point_theta = input("Enter the start orientation \n")
+# start = (int(start_point_x), int(start_point_y), int(start_point_theta))
+start = (100, 100, 0)
 while if_obstacle((start[0], start[1])):
     print("These coordinates lie inside the obstacle space. Please enter new values\n")
     start_point_x = input("Enter the x-coordinate of the start point \n")
     start_point_y = input("Enter the y-coordinate of the start point \n")
     start_point_theta = input("Enter the start orientation \n")
     start = (int(start_point_x), int(start_point_y), int(start_point_theta))
-print("_____________________________________________________________________________")
-goal_point_x = input("Enter the x-coordinate of the goal point \n")
-goal_point_y = input("Enter the y-coordinate of the goal point \n")
-goal_point_orien = input("Enter the goal orientation \n")
-goal = (int(goal_point_x), int(goal_point_y), int(goal_point_orien))
+# print("_____________________________________________________________________________")
+# goal_point_x = input("Enter the x-coordinate of the goal point \n")
+# goal_point_y = input("Enter the y-coordinate of the goal point \n")
+# goal_point_orien = input("Enter the goal orientation \n")
+# goal = (int(goal_point_x), int(goal_point_y), int(goal_point_orien))
+goal = (500, 100, 0)
 while if_obstacle((goal[0], goal[1])):
     print("These coordinates lie inside the obstacle space. Please enter new values\n")
     goal_point_x = input("Enter the x-coordinate of the goal point \n")
     goal_point_y = input("Enter the y-coordinate of the goal point \n")
     goal_point_orien = input("Enter the goal orientation \n")
     goal = (int(goal_point_x), int(goal_point_y), int(goal_point_orien))
-print("_____________________________________________________________________________")
+# print("_____________________________________________________________________________")
 goal_x = goal[0]
 goal_y = goal[1]
 
@@ -124,41 +113,39 @@ goal_y = goal[1]
 def cost(Xi,Yi,Thetai,UL,UR):
     # Thetai = Thetai % 360
     t = 0
-    r = 0.038
-    L = 0.354
+    r = 0.38
+    L = 3.54
     # r = 3.8
     # L = 3.54
     dt = 0.1
     Xn=Xi
     Yn=Yi
     Thetan = 3.14 * Thetai / 180
-# Xi, Yi,Thetai: Input point's coordinates
-# Xs, Ys: Start point coordinates for plot function
-# Xn, Yn, Thetan: End point coordintes
+    # Xi, Yi,Thetai: Input point's coordinates
+    # Xs, Ys: Start point coordinates for plot function
+    # Xn, Yn, Thetan: End point coordintes
     D=0
     while t<1:
         t = t + dt
         Xs = Xn
         Ys = Yn
+        # input = (Xn, Yn, total_bloat)
+        if if_obstacle((Xn, Yn)):
+            break
+
         Xn += 0.5*r * (UL + UR) * math.cos(Thetan) * dt
         Yn += 0.5*r * (UL + UR) * math.sin(Thetan) * dt
         Thetan += (r / L) * (UR - UL) * dt
         
         # plt.plot([Xs, Xn], [Ys, Yn], color="blue")        
         D=D+ math.sqrt(math.pow((0.5 * r * (UL + UR) * math.cos(Thetan) * dt), 2) + math.pow((0.5 * r * (UL + UR) * math.sin(Thetan) * dt), 2))
-        # Xn += 0.5*r * (UL + UR) * math.cos(Thetan) * dt
-        # Yn += 0.5*r * (UL + UR) * math.sin(Thetan) * dt
-        # Xn = Xn + Delta_Xn
-        # Yn = Yn + Delta_Yn
-    Thetan = 180 *(Thetan)/3.14
-    
-    if abs(Thetan) > 360:
+        
+    Thetan = 180 *(Thetan)/3.14    
+    if abs(Thetan) >= 360:
         Thetan = 360 - abs(Thetan)               
     return Xn, Yn, Thetan, D   
 
-
-# Defining the 5 moments spanning 120deg  
-def one(node, left_speed, right_speed):
+def action_move(node, left_speed, right_speed):
     modified_node = copy.deepcopy(node)
     modi_x, modi_y, modi_theta, c2c = cost(modified_node[3][0], modified_node[3][1], modified_node[3][2], left_speed, right_speed)
 
@@ -169,19 +156,20 @@ def one(node, left_speed, right_speed):
     modi_y = rounded_value(modi_y)
     modified_child = (modi_x, modi_y, modi_theta)
     c2c = c2c + modified_node[1]
+    c2c = rounded_value(c2c)
     cost_to_go = dist((modi_x, modi_y), (goal_x, goal_y))
-
-    total_cost = c2c + cost_to_go
+    cost_to_go = rounded_value(cost_to_go)
+    total_cost = c2c + 2*cost_to_go
     total_cost = rounded_value(total_cost)
     passed_node = a_star_node_create(
         total_cost, c2c, parent_node, modified_child)
     return passed_node
 
 def rounded_value(input):
-    # if input % 0.2 != 0:
-    #     input = (np.round(input/threshold))*threshold
+    if input % 0.2 != 0:
+        input = (np.round(input/threshold))*threshold
 
-    input = round(input, 1)
+    # input = round(input, 2)
     return input
 
 plotting = {}
@@ -189,9 +177,9 @@ def Astar_algoritm(start, goal):
 
     initial_cost_to_go = np.sqrt((goal[0]-start[0])**2 + (goal[1]-start[1])**2)
 
-    initial_total_cost = initial_cost_to_go + 0
+    initial_total_cost = 2*initial_cost_to_go + 0
 
-    zeros = np.zeros((int(6000/threshold), int(2000/threshold), int(360/30)))
+    zeros = np.zeros((int(600/threshold), int(200/threshold)))
 
     start_pose = a_star_node_create(initial_total_cost, 0, None, start)
 
@@ -232,13 +220,12 @@ def Astar_algoritm(start, goal):
 
         for action in actions:
             # Get the modified node and its total cost and cost to come from current node
-            child_node = one(current_node, action[0], action[1])
+            child_node = action_move(current_node, action[0], action[1])
             print(child_node)
             temp_node = child_node[3]
             # If the node is not in the the visited closed dictionary, then update the parent based on cost
-            if zeros[int(temp_node[0]/threshold)][int(temp_node[1]/threshold)][int(temp_node[2]/30)] == 0:
-                zeros[int(temp_node[0]/threshold)][int(temp_node[1]/threshold)
-                                            ][int(temp_node[2]/30)] = 1
+            if zeros[int(temp_node[0]/threshold)][int(temp_node[1]/threshold)] == 0:
+                zeros[int(temp_node[0]/threshold)][int(temp_node[1]/threshold)] = 1
                 if if_obstacle((temp_node[0], temp_node[1])) == False:
                     total_cost = child_node[0]
                     if child_node[3] not in visited_close_list:
@@ -264,24 +251,20 @@ for i in range(len(generated_path)):
     path_x_coord.append(generated_path[i][0])
     path_y_coord.append(generated_path[i][1])
 
-fig, ax = plt.subplots(figsize=(6000, 2000))
-# Triangle = patch.Polygon([(460, 25), (460, 225), (510, 125)], linewidth=1, edgecolor='y', facecolor='y')
-Rectangle_Bottom = patch.Rectangle((2500, 0), 150, 1250, linewidth=1, edgecolor='y', facecolor='y')
-Rectangle_Top = patch.Rectangle((1500, 750), 150, 1250, linewidth=1, edgecolor='y', facecolor='y')
-# Hexagon = patch.RegularPolygon((300, 125), 6, 75, linewidth=1, edgecolor='y', facecolor='y')
-Circle = patch.Circle((4000, 1100), 500, 75, linewidth=1, edgecolor='y', facecolor='y')
+fig, ax = plt.subplots(figsize=(600, 200))
+Rectangle_Bottom = patch.Rectangle((250, 0), 15, 125, linewidth=1, edgecolor='y', facecolor='y')
+Rectangle_Top = patch.Rectangle((150, 75), 15, 125, linewidth=1, edgecolor='y', facecolor='y')
+Circle = patch.Circle((400, 110), 50, linewidth=1, edgecolor='y', facecolor='y')
 # Plotting the obstacles
-# ax.add_patch(Triangle)
 ax.add_patch(Rectangle_Bottom)
 ax.add_patch(Rectangle_Top)
-# ax.add_patch(Hexagon)
 ax.add_patch(Circle)
 
 # Plotting the path
 plt.xlabel('X-Axis')
 plt.ylabel('Y-Axis')
 plt.title("Visualizing Explored Nodes through A* Algorithm")
-plt.axis([0, 6000, 0, 2000])
+plt.axis([0, 600, 0, 200])
 
 x_temp = 0
 y_temp = 0
